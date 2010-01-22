89f8f03582f7789c3920ca3dd411cba9c1a4549d
amccausl <amccausl@85b4c425-2b9f-47e2-b495-857cd3fc258c>
2010-01-22 00:33:01 +0000

    Added documentation.
    git-svn-id: svn+ssh://csclub.uwaterloo.ca/users/amccausl/code/svn@26 85b4c425-2b9f-47e2-b495-857cd3fc258c

--- src/crawler.erl
+++ src/crawler.erl
@@ -1,13 +1,46 @@
+%%% @author Alex McCausland <alex.mccausland@gmail.com>
+%%% @doc 
+%%% This is a learning excerize in the language.
+%%%
+%%% start() -> Parse parameters from command line, call init, run router
+%%% init() -> configure http client, load config, start router (register(router, Router))
+%%% router() -> Store rules for downloader
+%%%  rule must include callbacks, save location (or undef), number of threads
+%%%  use register(Name, PID) and whereis(Name) to find actor for each rule
+%%% rule_actor(Rule) -> knows action to take for file, spawns threads to take action,
+%%%  possible actions: save(fun filename) save url to disk, crawl([Tags]) takes array of tags to call router for
+%%%  creates number of threads
+%%%  accepts messages from threads ('DOWN', Ref, process, Pid2, Reason), and from router (url, URL)
+%%% downloader 
+%%% crawler
+%%% @end
+
+% main actor to hit param url, download threads
+
+% as thread is parsed, spawn or pass messages as needed to download contents
+
+% each parser should register with the name from the config
+
+%get_filename( {URL, Headers} ) ->
+
 -module( crawler ).
--export( [ receiver/0, giver/1, start/0, test_parse/1, get_page/1 ] ).
+-export( [ start/0, start/1, get_page/1 ] ).
 -include_lib("xmerl/include/xmerl.hrl").
 
 % start with url on cmd line, maybe some preprocessing
 start() ->
-  inets:start(),
-  RouterPID = spawn( crawler, router, [] ),
-  {ok, [URLs]} = init:get_arguments( url ).
-%start_test(URL) ->
+  init(),
+  {ok, [URLs]} = init:get_arguments( url ),
+  RouterPID = spawn( crawler, router, [] );
+start_test(URL) ->
+  init(),
+  RouterPID = spawn( crawler, router, [] ).
+
+init() ->
+  inets:start().
+  % configure http client
+  % load config
+  % create processes for each parser
 
 router() ->
   receive
@@ -15,47 +48,28 @@ router() ->
     %{ img, IMG } ->
   end.
 
-% main actor to hit param url, download threads
-
-% as thread is parsed, spawn or pass messages as needed to download contents
-
-
-
-receiver() ->
+rule_actor(Rule, NumThreads) ->
   receive
-    diediedie -> ok;
-    { name, Name } -> io:format( "hello, ~s~n", [ Name ] ), receiver()
+    { url, URL } ->
+	  
   end.
-giver( ReceiverPid ) ->
-  ReceiverPid ! { name, "Andre" },
-  ReceiverPid ! { name, "Linux.conf.au" },
-  ReceiverPid ! diediedie.
-start() ->
-  ReceiverPid = spawn( hello_concurrent, receiver, [] ),
-  spawn( hello_concurrent, giver, [ ReceiverPid ] ),
-  start_finished.
 
-test_parse(URL) ->
-  inets:start(),
-  { ok, {_Status, _Headers, Body}} = http:request(URL),
-  xmerl_scan:string( xmerl_ucs:to_utf8(Body) ).
-  
+receiver(Rule, Queue, Threads) ->
+  % if threads > 0, queue not empty, process from queue
+  receive
+    { url, URL } ->
+      downloader(Rule, URL),
+      receiver(Rule, Queue, Threads - 1);
+    {'DOWN', Ref, process, Pid, Reason} ->
+	  receiver( Rule, Threads + 1 )
+  end.
+
+%downloader(Rule, URL) ->
+%crawler(Rule, URL) ->
 
 get_page(URL) ->
-  %% Don't know why I need the following line
-  %% mentioned in inets documentation
-  inets:start(),
   process_page( http:request(URL) ).
-
-%get_filename( {URL, Headers} ) ->
 
 process_page( { ok, {_Status, _Headers, Body }} ) -> lists:subtract(lists:subtract(Body, "// [ "), "] ");
 process_page( {error,no_scheme} ) -> io:format( "No scheme error" ).
 
-
-% start() -> Parse parameters from command line, start router, configure http client
-% router() -> Store rules for downloader
- % rule must include callbacks, save location (or undef), 
-% parse_with_callback(Body) -> Attempt to scan HTML to xmerl data structure
- % when tag with callback defined is closed, pass message
- % callbacks store PID of router, pass messages back up
