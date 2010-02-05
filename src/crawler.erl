65795de15d7afa1d4caa064e0c8e18ddb42639c1
amccausl <amccausl@85b4c425-2b9f-47e2-b495-857cd3fc258c>
2010-02-05 17:28:27 +0000

    Added url parsing.
    git-svn-id: svn+ssh://csclub.uwaterloo.ca/users/amccausl/code/svn@29 85b4c425-2b9f-47e2-b495-857cd3fc258c

--- src/crawler.erl
+++ src/crawler.erl
@@ -25,7 +25,6 @@
 
 -module( crawler ).
 -export( [ start/0, start/1, get_page/1, dispatcher_init/1 ] ).
--include_lib("xmerl/include/xmerl.hrl").
 
 % start with url on cmd line, maybe some preprocessing
 start() ->
@@ -86,3 +85,95 @@ get_page(URL) ->
 
 process_page( { ok, {_Status, _Headers, Body }} ) -> lists:subtract(lists:subtract(Body, "// [ "), "] ");
 process_page( {error,no_scheme} ) -> io:format( "No scheme error" ).
+
+crawl( ParentID, RuleID, Url ) ->
+  {ok, {_Status, _Headers, HTML}} = http:request(Url),
+  RelativeUrls = parse_url( HTML, [] ),
+  Urls = lists:map(fun(X) -> url_makeAbsolute(X) end, RelativeUrls), 
+  crawl_re(ParentID, Urls, Threads, 1).
+
+crawl_re( ParentID, [], Threads, ThreadCount, Index ) ->
+	receive
+		{callback, PID, Index, Url, Filename} ->
+			crawl_re( ParentID, [],  )lists;
+    	{'DOWN', Ref, process, Pid, Reason} ->	
+	end;
+crawl_re( ParentID, [Url|Urls], Threads, ThreadCount, Index ) when ThreadCount > 0 ->
+	NewThread = { spawn( crawler, )},
+	crawl_re( ParentID, Urls, [NewThread|Threads], ThreadCount - 1, Index + 1 ).
+
+% Final
+
+rule_crawl(Rule, [], Threads, ThreadCount) ->
+	receive
+		{callback, Pid, Index, Url} ->
+rule_crawl(Rule, Queue, Threads, ThreadCount) when ThreadCount > 0 ->
+	.
+
+rule_save(Rule, [], Threads, ThreadCount) when ThreadCount > 0 ->
+	receive
+		{url, Request} ->
+	end;
+rule_save(Rule, Queue, Threads, ThreadCount) ->
+	receive
+		{url, Request} -> rule_save( Rule, [Request|Queue], Threads, ThreadCount);
+		{callback, Pid, Index, }
+	end;
+rule_save(Rule, Queue, Threads, ThreadCount) when ThreadCount > 0 ->
+	receive
+	end.
+
+save_thread(Url, Filename) ->
+	.
+
+rule_rewrite(Rule) ->
+	receive
+		{url, Request} ->
+	end.
+
+rewrite(Url, []) -> {ok}. 
+rewrite(Url, [Translation|Rest]) ->
+	
+
+get_rule_process(Url) ->
+	.
+
+% Crawl helpers
+parse_quoted(Input) ->
+	[Quote|Tail] = Input,
+	Quoted = lists:takewhile(fun(X) -> X =/= Quote end, Tail),
+	{Quoted, lists:sublist(Tail, length(Quoted) + 1, 100000)}.
+parse_url( [], Urls ) ->
+	lists:reverse(Urls);
+parse_url( HTML, Urls ) ->
+	case lists:prefix("src=", HTML) of
+		true ->
+			{Url, Remaining} = parse_quoted(lists:sublist(HTML, 5, 100000)),
+			parse_url(Remaining, [Url|Urls]);
+		false ->
+			case lists:prefix("href=", HTML) of
+				true ->
+					{Url, Remaining} = parse_quoted(lists:sublist(HTML, 6, 100000)),
+					parse_url(Remaining, [Url|Urls]);
+				false ->
+					[_|T] = HTML,
+					parse_url(T, Urls)
+			end
+	end.
+url_makeAbsolute( SourceUrl, Url ) ->
+	case lists:suffix("http://", Url) of
+		true -> Url;
+		false ->
+			Dir = url_getDirectory( SourceUrl ),
+			Dir ++ Url
+	end.
+url_getDirectory( Url )	->
+	% take everything before the '?'
+	Base = lists:takewhile(fun(X) -> X =/= 63 end, Url),
+	case lists:suffix("/", Base) of
+		true ->
+			Base;
+		false ->
+			RUrl = lists:reverse(Base),
+			lists:reverse(lists:dropwhile(fun(X) -> X =/= 47 end, RUrl))
+	end.
--- src/examples/demo.erl
+++ src/examples/demo.erl
@@ -1,5 +1,5 @@
 -module( demo ).
--export( [test_string_generate/3, test_subpattern/0, extract_atoms/1, template_string/0, test_compile/0, load_file/0, trans_compile/1, trans_run/2] ).
+-export( [test_string_generate/3, test_subpattern/0, extract_atoms/1, template_string/0, test_compile/0, load_file/0, trans_compile/1, trans_run/2, parse_url/2, url_makeAbsolute/2, url_getDirectory/1] ).
 
 % generate filename from formula and regex matched
 test_string_generate(String, Regex, Template) ->
@@ -18,7 +18,7 @@ test_subpattern() ->
 
 extract_atoms(Regex) ->
   case re:run( Regex, ets:lookup_element(?MODULE, namedRegex, 0), [global, {capture, [1], list}] ) of
-    {match, Subpatterns} -> map(fun list_to_atom/1, lists:append(Subpatterns));
+    {match, Subpatterns} -> lists:map(fun list_to_atom/1, lists:append(Subpatterns));
     _                    -> []
   end.
 
@@ -30,16 +30,13 @@ test_compile() ->
   {ok, MP} = re:compile("http://(?<host>.*)", []),
   re:run( "http://google.ca", MP ).
 
-map(_, []) -> [];
-map(F, [H|T]) -> [F(H)|map(F,T)].
-
 load_file() ->
   ConfFile = filename:join(["..", "..", "config", "crawler_dispatcher.conf"]),
   {ok, Terms} = file:consult(ConfFile),
   {ok, NamedRegex} = re:compile("\\?\\<([A-Za-z_0-9]+)\\>", []),
   ets:new(?MODULE, [named_table, public]),
   ets:insert(?MODULE, [ {namedRegex, NamedRegex} ]),
-  Rules = map(fun preprocess/1, Terms),
+  Rules = lists:map(fun preprocess/1, Terms),
   ets:insert(?MODULE, [ {rules, Rules} ]),
   {ok}.
 
@@ -52,3 +49,43 @@ trans_compile({Regex, Filename}) ->
 trans_run(Input, {Regex, Atoms, Template}) ->
   {match, Subpatterns} = re:run(Input, Regex, [global, {capture, Atoms, list}]),
   sgte:render_str(Template, lists:zip(Atoms, Subpatterns)).
+
+% TODO: after parse_url returns, we must make all the urls absolute
+
+parse_quoted(Input) ->
+	[Quote|Tail] = Input,
+	Quoted = lists:takewhile(fun(X) -> X =/= Quote end, Tail),
+	{Quoted, lists:sublist(Tail, length(Quoted) + 1, 100000)}.
+
+parse_url( [], Urls ) ->
+	lists:reverse(Urls);
+parse_url( HTML, Urls ) ->
+	case lists:prefix("src=", HTML) of
+		true ->
+			{Url, Remaining} = parse_quoted(lists:sublist(HTML, 5, 100000)),
+			parse_url(Remaining, [Url|Urls]);
+		false ->
+			case lists:prefix("href=", HTML) of
+				true ->
+					{Url, Remaining} = parse_quoted(lists:sublist(HTML, 6, 100000)),
+					parse_url(Remaining, [Url|Urls]);
+				false ->
+					[_|T] = HTML,
+					parse_url(T, Urls)
+			end
+	end.
+
+
+url_makeAbsolute( SourceUrl, Urls ) ->
+	Dir = url_getDirectory( SourceUrl ),
+	lists:map(fun(X) -> Dir ++ X end, Urls).
+url_getDirectory( Url )	->
+	% take everything before the '?'
+	Base = lists:takewhile(fun(X) -> X =/= 63 end, Url),
+	case lists:suffix("/", Base) of
+		true ->
+			Base;
+		false ->
+			RUrl = lists:reverse(Base),
+			lists:reverse(lists:dropwhile(fun(X) -> X =/= 47 end, RUrl))
+	end.
